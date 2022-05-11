75ac4c5663e0c79f225b0de811789867818400c2
hoyong <mooroom.dev@gmail.com>
2022-05-11 22:51:01 +0900

    feat: 그리드 생성 및 클릭 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const noteCount = 3;
+const beatCount = 8;
+
+const noteWidth = 50;
+const noteHeight = 30;
+
+const gridBorderWidth = 6;
+
+const laneWidth = gridBorderWidth * (beatCount + 1) + noteWidth * beatCount;
+const laneHeight = gridBorderWidth * (noteCount + 1) + noteHeight * noteCount;
+
+const gridHorizontal = Array.from(
+  { length: noteCount + 1 },
+  (v, k) => (noteHeight + gridBorderWidth) * k
+);
+
+const gridVertical = Array.from(
+  { length: beatCount + 1 },
+  (v, k) => (noteWidth + gridBorderWidth) * k
+);
+
+const gridData = Array(noteCount).fill(Array(beatCount).fill(0));
+console.log(gridData);
 
 function App() {
+  const editorRef = useRef<SVGSVGElement>(null);
+
+  const handleClickEditor = (e: React.MouseEvent) => {
+    const $target = e.target as SVGSVGElement;
+    const { x: offsetX, y: offsetY } = $target.getBoundingClientRect();
+    const gridX = e.clientX - offsetX;
+    const gridY = e.clientY - offsetY;
+    console.log(`x: ${gridX}, y: ${gridY}`);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <svg
+        className="editor"
+        width={laneWidth}
+        height={laneHeight}
+        onClick={handleClickEditor}
+      >
+        {gridHorizontal.map((v) => (
+          <rect
+            fill="#666"
+            width={laneWidth}
+            height={gridBorderWidth}
+            x={0}
+            y={v}
+          />
+        ))}
+        {gridVertical.map((v) => (
+          <rect
+            fill="#666"
+            width={gridBorderWidth}
+            height={laneHeight}
+            x={v}
+            y={0}
+          />
+        ))}
+      </svg>
     </div>
   );
 }
