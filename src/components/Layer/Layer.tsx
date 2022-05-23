00661b12324362dceabbd60fc0c78ef2825c8621
hoyong <mooroom.dev@gmail.com>
2022-05-23 19:21:45 +0900

    fix: 그리드 간격 수정

--- src/components/Layer/Layer.tsx
+++ src/components/Layer/Layer.tsx
@@ -6,50 +6,51 @@ import { StyledComponent } from "styled-components";
 const C4_MAJOR = Scale.get("C4 major").notes;
 const C5_MAJOR = Scale.get("C5 major").notes;
 const C6_MAJOR = Scale.get("C6 major").notes;
-const notes = [...C4_MAJOR, ...C5_MAJOR, ...C6_MAJOR];
+const notes = [...C4_MAJOR, ...C5_MAJOR, "C6"];
 
 const rows = notes.length;
 const cols = 32;
 
 const borderWidth = 2;
 
-const gridYLines = Array.from({ length: rows + 1 }, (_, i) => 20 * i);
+const gridYLines = Array.from({ length: rows + 1 }, (_, i) => 30 * i);
 const gridXLines = Array.from({ length: cols + 1 });
 
 export default function Layer() {
   const [gridXLines, setGridXLines] = useState<number[]>([]);
 
-  const editorRef = useRef<SVGSVGElement>(null);
+  const layerRef = useRef<HTMLDivElement>(null);
 
   const handleResize = () => {
-    const editor = editorRef.current;
-    if (editor) {
-      const { width: editorWidth } = editor.getBoundingClientRect();
-      const colWidth = Math.max(50, (editorWidth - borderWidth * cols) / cols);
+    const layer = layerRef.current;
+    if (layer) {
+      const { width: layerWidth } = layer.getBoundingClientRect();
+      const colWidth = Math.max(20, (layerWidth - 60) / cols);
+      console.log(colWidth);
       const newXLines = Array.from(
         { length: cols + 1 },
-        (_, i) => colWidth * i
+        (_, i) => colWidth * i + 60
       );
       setGridXLines(newXLines);
     }
   };
 
   useEffect(() => {
-    console.log("Layer!");
+    handleResize();
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return (
-    <S.LayerBlock>
+    <S.LayerBlock ref={layerRef}>
       <S.WidgetContainer>
-        <S.EditorBlock ref={editorRef}>
+        <S.EditorBlock height={452} overflow="scroll">
           <S.LaneContainer></S.LaneContainer>
           <S.GridContainer>
             {gridYLines.map((v, i) => (
               <rect
                 key={v + i}
-                fill="#000"
+                fill="#eee"
                 width="100%"
                 height={borderWidth}
                 x={0}
@@ -59,7 +60,7 @@ export default function Layer() {
             {gridXLines.map((v, i) => (
               <rect
                 key={v + i}
-                fill="#000"
+                fill="#eee"
                 width={borderWidth}
                 height="100%"
                 x={v}
